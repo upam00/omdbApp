cf1360bfc7c6d8aad99dfc6a044fabcbb892b2cd
Upam <upam.sarmah@gmail.com>
2025-02-25 15:26:51 +0530

    Consumed search api and fixed some styling.

--- script.js
+++ script.js
@@ -1,286 +1,6 @@
-const data  = [
-    {
-        "Title": "The Fast and the Furious",
-        "Year": "2001",
-        "Rated": "PG-13",
-        "Released": "22 Jun 2001",
-        "Runtime": "106 min",
-        "Genre": "Action, Crime, Thriller",
-        "Director": "Rob Cohen",
-        "Writer": "Ken Li, Gary Scott Thompson, Erik Bergquist",
-        "Actors": "Vin Diesel, Paul Walker, Michelle Rodriguez",
-        "Plot": "Los Angeles police officer Brian O'Conner must decide where his loyalty really lies when he becomes enamored with the street racing world he has been sent undercover to end it.",
-        "Language": "English, Spanish",
-        "Country": "United States, Germany",
-        "Awards": "11 wins & 18 nominations total",
-        "Poster": "https://m.media-amazon.com/images/M/MV5BZGRiMDE1NTMtMThmZS00YjE4LWI1ODQtNjRkZGZlOTg2MGE1XkEyXkFqcGc@._V1_SX300.jpg",
-        "Ratings": [
-            {
-                "Source": "Internet Movie Database",
-                "Value": "6.8/10"
-            },
-            {
-                "Source": "Rotten Tomatoes",
-                "Value": "55%"
-            },
-            {
-                "Source": "Metacritic",
-                "Value": "58/100"
-            }
-        ],
-        "Metascore": "58",
-        "imdbRating": "6.8",
-        "imdbVotes": "432,958",
-        "imdbID": "tt0232500",
-        "Type": "movie",
-        "DVD": "N/A",
-        "BoxOffice": "$144,745,925",
-        "Production": "N/A",
-        "Website": "N/A",
-        "Response": "True"
-    },
-    {
-        "Title": "The Fast and the Furious",
-        "Year": "2001",
-        "Rated": "PG-13",
-        "Released": "22 Jun 2001",
-        "Runtime": "106 min",
-        "Genre": "Action, Crime, Thriller",
-        "Director": "Rob Cohen",
-        "Writer": "Ken Li, Gary Scott Thompson, Erik Bergquist",
-        "Actors": "Vin Diesel, Paul Walker, Michelle Rodriguez",
-        "Plot": "Los Angeles police officer Brian O'Conner must decide where his loyalty really lies when he becomes enamored with the street racing world he has been sent undercover to end it.",
-        "Language": "English, Spanish",
-        "Country": "United States, Germany",
-        "Awards": "11 wins & 18 nominations total",
-        "Poster": "https://m.media-amazon.com/images/M/MV5BZGRiMDE1NTMtMThmZS00YjE4LWI1ODQtNjRkZGZlOTg2MGE1XkEyXkFqcGc@._V1_SX300.jpg",
-        "Ratings": [
-            {
-                "Source": "Internet Movie Database",
-                "Value": "6.8/10"
-            },
-            {
-                "Source": "Rotten Tomatoes",
-                "Value": "55%"
-            },
-            {
-                "Source": "Metacritic",
-                "Value": "58/100"
-            }
-        ],
-        "Metascore": "58",
-        "imdbRating": "6.8",
-        "imdbVotes": "432,958",
-        "imdbID": "tt0232500",
-        "Type": "movie",
-        "DVD": "N/A",
-        "BoxOffice": "$144,745,925",
-        "Production": "N/A",
-        "Website": "N/A",
-        "Response": "True"
-    },
-    {
-        "Title": "The Fast and the Furious",
-        "Year": "2001",
-        "Rated": "PG-13",
-        "Released": "22 Jun 2001",
-        "Runtime": "106 min",
-        "Genre": "Action, Crime, Thriller",
-        "Director": "Rob Cohen",
-        "Writer": "Ken Li, Gary Scott Thompson, Erik Bergquist",
-        "Actors": "Vin Diesel, Paul Walker, Michelle Rodriguez",
-        "Plot": "Los Angeles police officer Brian O'Conner must decide where his loyalty really lies when he becomes enamored with the street racing world he has been sent undercover to end it.",
-        "Language": "English, Spanish",
-        "Country": "United States, Germany",
-        "Awards": "11 wins & 18 nominations total",
-        "Poster": "https://m.media-amazon.com/images/M/MV5BZGRiMDE1NTMtMThmZS00YjE4LWI1ODQtNjRkZGZlOTg2MGE1XkEyXkFqcGc@._V1_SX300.jpg",
-        "Ratings": [
-            {
-                "Source": "Internet Movie Database",
-                "Value": "6.8/10"
-            },
-            {
-                "Source": "Rotten Tomatoes",
-                "Value": "55%"
-            },
-            {
-                "Source": "Metacritic",
-                "Value": "58/100"
-            }
-        ],
-        "Metascore": "58",
-        "imdbRating": "6.8",
-        "imdbVotes": "432,958",
-        "imdbID": "tt0232500",
-        "Type": "movie",
-        "DVD": "N/A",
-        "BoxOffice": "$144,745,925",
-        "Production": "N/A",
-        "Website": "N/A",
-        "Response": "True"
-    },
-    {
-        "Title": "The Fast and the Furious",
-        "Year": "2001",
-        "Rated": "PG-13",
-        "Released": "22 Jun 2001",
-        "Runtime": "106 min",
-        "Genre": "Action, Crime, Thriller",
-        "Director": "Rob Cohen",
-        "Writer": "Ken Li, Gary Scott Thompson, Erik Bergquist",
-        "Actors": "Vin Diesel, Paul Walker, Michelle Rodriguez",
-        "Plot": "Los Angeles police officer Brian O'Conner must decide where his loyalty really lies when he becomes enamored with the street racing world he has been sent undercover to end it.",
-        "Language": "English, Spanish",
-        "Country": "United States, Germany",
-        "Awards": "11 wins & 18 nominations total",
-        "Poster": "https://m.media-amazon.com/images/M/MV5BZGRiMDE1NTMtMThmZS00YjE4LWI1ODQtNjRkZGZlOTg2MGE1XkEyXkFqcGc@._V1_SX300.jpg",
-        "Ratings": [
-            {
-                "Source": "Internet Movie Database",
-                "Value": "6.8/10"
-            },
-            {
-                "Source": "Rotten Tomatoes",
-                "Value": "55%"
-            },
-            {
-                "Source": "Metacritic",
-                "Value": "58/100"
-            }
-        ],
-        "Metascore": "58",
-        "imdbRating": "6.8",
-        "imdbVotes": "432,958",
-        "imdbID": "tt0232500",
-        "Type": "movie",
-        "DVD": "N/A",
-        "BoxOffice": "$144,745,925",
-        "Production": "N/A",
-        "Website": "N/A",
-        "Response": "True"
-    },
-    {
-        "Title": "The Fast and the Furious",
-        "Year": "2001",
-        "Rated": "PG-13",
-        "Released": "22 Jun 2001",
-        "Runtime": "106 min",
-        "Genre": "Action, Crime, Thriller",
-        "Director": "Rob Cohen",
-        "Writer": "Ken Li, Gary Scott Thompson, Erik Bergquist",
-        "Actors": "Vin Diesel, Paul Walker, Michelle Rodriguez",
-        "Plot": "Los Angeles police officer Brian O'Conner must decide where his loyalty really lies when he becomes enamored with the street racing world he has been sent undercover to end it.",
-        "Language": "English, Spanish",
-        "Country": "United States, Germany",
-        "Awards": "11 wins & 18 nominations total",
-        "Poster": "https://m.media-amazon.com/images/M/MV5BZGRiMDE1NTMtMThmZS00YjE4LWI1ODQtNjRkZGZlOTg2MGE1XkEyXkFqcGc@._V1_SX300.jpg",
-        "Ratings": [
-            {
-                "Source": "Internet Movie Database",
-                "Value": "6.8/10"
-            },
-            {
-                "Source": "Rotten Tomatoes",
-                "Value": "55%"
-            },
-            {
-                "Source": "Metacritic",
-                "Value": "58/100"
-            }
-        ],
-        "Metascore": "58",
-        "imdbRating": "6.8",
-        "imdbVotes": "432,958",
-        "imdbID": "tt0232500",
-        "Type": "movie",
-        "DVD": "N/A",
-        "BoxOffice": "$144,745,925",
-        "Production": "N/A",
-        "Website": "N/A",
-        "Response": "True"
-    },
-    {
-        "Title": "The Fast and the Furious",
-        "Year": "2001",
-        "Rated": "PG-13",
-        "Released": "22 Jun 2001",
-        "Runtime": "106 min",
-        "Genre": "Action, Crime, Thriller",
-        "Director": "Rob Cohen",
-        "Writer": "Ken Li, Gary Scott Thompson, Erik Bergquist",
-        "Actors": "Vin Diesel, Paul Walker, Michelle Rodriguez",
-        "Plot": "Los Angeles police officer Brian O'Conner must decide where his loyalty really lies when he becomes enamored with the street racing world he has been sent undercover to end it.",
-        "Language": "English, Spanish",
-        "Country": "United States, Germany",
-        "Awards": "11 wins & 18 nominations total",
-        "Poster": "https://m.media-amazon.com/images/M/MV5BZGRiMDE1NTMtMThmZS00YjE4LWI1ODQtNjRkZGZlOTg2MGE1XkEyXkFqcGc@._V1_SX300.jpg",
-        "Ratings": [
-            {
-                "Source": "Internet Movie Database",
-                "Value": "6.8/10"
-            },
-            {
-                "Source": "Rotten Tomatoes",
-                "Value": "55%"
-            },
-            {
-                "Source": "Metacritic",
-                "Value": "58/100"
-            }
-        ],
-        "Metascore": "58",
-        "imdbRating": "6.8",
-        "imdbVotes": "432,958",
-        "imdbID": "tt0232500",
-        "Type": "movie",
-        "DVD": "N/A",
-        "BoxOffice": "$144,745,925",
-        "Production": "N/A",
-        "Website": "N/A",
-        "Response": "True"
-    },
-    {
-        "Title": "The Fast and the Furious",
-        "Year": "2001",
-        "Rated": "PG-13",
-        "Released": "22 Jun 2001",
-        "Runtime": "106 min",
-        "Genre": "Action, Crime, Thriller",
-        "Director": "Rob Cohen",
-        "Writer": "Ken Li, Gary Scott Thompson, Erik Bergquist",
-        "Actors": "Vin Diesel, Paul Walker, Michelle Rodriguez",
-        "Plot": "Los Angeles police officer Brian O'Conner must decide where his loyalty really lies when he becomes enamored with the street racing world he has been sent undercover to end it.",
-        "Language": "English, Spanish",
-        "Country": "United States, Germany",
-        "Awards": "11 wins & 18 nominations total",
-        "Poster": "https://m.media-amazon.com/images/M/MV5BZGRiMDE1NTMtMThmZS00YjE4LWI1ODQtNjRkZGZlOTg2MGE1XkEyXkFqcGc@._V1_SX300.jpg",
-        "Ratings": [
-            {
-                "Source": "Internet Movie Database",
-                "Value": "6.8/10"
-            },
-            {
-                "Source": "Rotten Tomatoes",
-                "Value": "55%"
-            },
-            {
-                "Source": "Metacritic",
-                "Value": "58/100"
-            }
-        ],
-        "Metascore": "58",
-        "imdbRating": "6.8",
-        "imdbVotes": "432,958",
-        "imdbID": "tt0232500",
-        "Type": "movie",
-        "DVD": "N/A",
-        "BoxOffice": "$144,745,925",
-        "Production": "N/A",
-        "Website": "N/A",
-        "Response": "True"
-    }]
+const data  = []
 
-    const yearSelect = document.getElementById("yearSelect");
+const yearSelect = document.getElementById("yearSelect");
 for (let year = new Date().getFullYear(); year >= 1900; year--) {
     let option = document.createElement("option");
     option.value = year;
@@ -291,58 +11,64 @@ for (let year = new Date().getFullYear(); year >= 1900; year--) {
 document.getElementById("searchButton").addEventListener("click", function() {
     const query = document.getElementById("searchBox").value;
     const year = document.getElementById("yearSelect").value;
-    let url = `https://www.omdbapi.com/?s=${query}&apikey=YOUR_API_KEY`;
+    let apiKey = sessionStorage.getItem("key");
+    let url = `https://www.omdbapi.com/?s=${query}&apikey=${apiKey}`;
     if (year) url += `&y=${year}`;
     
-    // fetch(url)
-    //     .then(response => response.json())
-    //     .then(data => {
-    //         const results = document.getElementById("movieResults");
-    //         const scrollContainer = document.getElementById("scrollContainer");
-    //         results.innerHTML = "";
-    //         if (data.Search) {
-    //             data.Search.forEach(movie => {
-    //                 const movieElement = document.createElement("div");
-    //                 movieElement.classList.add("movie-tile");
-    //                 movieElement.innerHTML = `
-    //                     <img src="${movie.Poster}" alt="${movie.Title}">
-    //                     <h2>${movie.Title} (${movie.Year})</h2>
-    //                     <p><strong>Type:</strong> ${movie.Type}</p>
-    //                 `;
-    //                 results.appendChild(movieElement);
-    //             });
-    //             scrollContainer.style.display = "flex";
-    //             document.querySelector(".scroll-btn.left").style.display = "block";
-    //             document.querySelector(".scroll-btn.right").style.display = "block";
-    //         } else {
-    //             results.innerHTML = "<p>No movies found.</p>";
-    //             scrollContainer.style.display = "none";
-    //         }
-    //     })
-    //     .catch(error => console.error("Error fetching data:", error));
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            const results = document.getElementById("movieResults");
+            const scrollContainer = document.getElementById("scrollContainer");
+            results.innerHTML = "";
+            if (data.Search) {
+                data.Search.forEach(movie => {
+                    const movieElement = document.createElement("div");
+                    movieElement.classList.add("movie-tile");
+                    movieElement.innerHTML = `
+                        <img src="${movie.Poster}" alt="${movie.Title}">
+                        <p><strong>${movie.Title}</strong></p>
+                        <p>(${movie.Year})</p>
+                        <p>Genre: ${movie.Genre}</p>
+                        <p>IMDB Rating:${movie.imdbRating}</p>
+                    `;
+                    movieElement.addEventListener("click",  function() {
+                        openPopup("https://www.youtube.com/embed/zSWdZVtXT7E?autoplay=1", movie);
+                    });
+                    results.appendChild(movieElement);
+                });
+                scrollContainer.style.display = "flex";
+                document.querySelector(".scroll-btn.left").style.display = "block";
+                document.querySelector(".scroll-btn.right").style.display = "block";
+            } else {
+                results.innerHTML = "<p>No movies found.</p>";
+                scrollContainer.style.display = "none";
+            }
+        })
+        .catch(error => console.error("Error fetching data:", error));
 
-    const results = document.getElementById("movieResults");
-    const scrollContainer = document.getElementById("scrollContainer");
-    results.innerHTML = "";
-    if(data) {
-        data.forEach(movie => {
-            const movieElement = document.createElement("div");
-            movieElement.classList.add("movie-tile");
-            movieElement.innerHTML = `
-                <img src="${movie.Poster}" alt="${movie.Title}">
-                <p>${movie.Title} (${movie.Year})</p>
-                <p><strong>Type:</strong> ${movie.Type}</p>
-            `;
-            movieElement.addEventListener("click",  function() {
-                openPopup("https://www.youtube.com/embed/zSWdZVtXT7E?autoplay=1", movie);
-            });
+    // const results = document.getElementById("movieResults");
+    // const scrollContainer = document.getElementById("scrollContainer");
+    // results.innerHTML = "";
+    // if(data) {
+    //     data.forEach(movie => {
+    //         const movieElement = document.createElement("div");
+    //         movieElement.classList.add("movie-tile");
+    //         movieElement.innerHTML = `
+    //             <img src="${movie.Poster}" alt="${movie.Title}">
+    //             <p>${movie.Title} (${movie.Year})</p>
+    //             <p><strong>Type:</strong> ${movie.Type}</p>
+    //         `;
+    //         movieElement.addEventListener("click",  function() {
+    //             openPopup("https://www.youtube.com/embed/zSWdZVtXT7E?autoplay=1", movie);
+    //         });
            
-            results.appendChild(movieElement);
-        });
-    }
-    scrollContainer.style.display = "flex";
-    document.querySelector(".scroll-btn.left").style.display = "block";
-    document.querySelector(".scroll-btn.right").style.display = "block";
+    //         results.appendChild(movieElement);
+    //     });
+    // }
+    // scrollContainer.style.display = "flex";
+    // document.querySelector(".scroll-btn.left").style.display = "block";
+    // document.querySelector(".scroll-btn.right").style.display = "block";
 
 });
 
